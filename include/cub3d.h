76a4cf8df44de4e3f62d30c7fa77dc6c02bd35a7
Kristina Nemcova <knemcova@c4r3s16.42tokyo.jp>
2025-04-13 16:15:54 +0900

    start

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,15 +6,19 @@
 /*   By: knemcova <knemcova@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/13 12:05:56 by knemcova          #+#    #+#             */
-/*   Updated: 2025/04/13 12:16:33 by knemcova         ###   ########.fr       */
+/*   Updated: 2025/04/13 14:32:12 by knemcova         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
+# include "../libft/libft.h"
 # include "../minilibx-linux/mlx.h"
 # include <fcntl.h>
 # include <limits.h>
+# include <stdio.h>
 # include <stdlib.h>
-# include <unistd.h> 
+# include <unistd.h>
+
+#endif
